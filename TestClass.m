7c33371b3a2540efe4b012ea91df083a324c431e
Sofie Ahlberg <sofa@UG.KTH.SE>
2021-03-09 10:12:05 +0100

    Improve documentation of test class in context of comments in the file

--- TestClass.m
+++ TestClass.m
@@ -39,38 +39,41 @@ classdef TestClass < matlab.unittest.TestCase
             expSolution=[1,1;1,2;2,1;2,2;3,1;3,2;4,1;4,2;5,1;5,2];
             testCase.verifyEqual(actSolution,expSolution);
             
-            %testing the dimension
+            %testing the dimension of a specific case
             actDim=size(cartesian_product([1:5],[1:2],[1:3]));
             expDim=[5*2*3, 3];
             testCase.verifyEqual(actDim,expDim)
         end
         function testcontainedIn(testCase)
-            %testcontainedIn testz the function containedIn in enviornment
-            %which return 1 if the first input argument is a point
-            %withinthe rectangle defined by the points in T.R=[xmin, ymin;
+            %testcontainedIn tests the function containedIn in enviornment
+            %which should return 1 if the first input argument is a point
+            %within the rectangle defined by the points in T.R=[xmin, ymin;
             %xmax, ymax] and 0 otherwise.
             T.R=[0 0; 1 1];
             
-            %test return of 1
+            %test return of 1(x in rectangle)
             actSolution=containedIn([0.5, 0.5],1,T);
             expSolution=1;
             testCase.verifyEqual(actSolution, expSolution);
             
-            %test return of 0
+            %test return of 0 (x outside of rectangle)
             actSolution=containedIn([2.5, 0.5],1,T);
             expSolution=0;
             testCase.verifyEqual(actSolution, expSolution)
         end
         function testWTSsimple(testCase)
             %testWTSsimple tests the function WTS_simpleconstruction in TS
-            %which returns an object with 6 properties describing a WTS.
+            %which should return an object with 6 properties describing a 
+            %WTS. 
+            
             env=env1(); %some example settings
             a=2; b=2;u=1; d=2; l=3; r=4;
-            
-            %test return of 1
             T=WTS_simpleconstruction(env.n, a,b, env.Pi, env.Lap, u,d,l,r,env.init);
             
             %test cases for corners of states
+            % rectangle1 should start in 0,0. all rectangles should be a
+            % wide and b high. there should be 5 rectangles in horizontal
+            % direction and 3 in vertical (giving 15 in total)
             actSolution=T.R(1,:,1);expSolution=[0,0];
             testCase.verifyEqual(actSolution, expSolution);
             actSolution=T.R(2,:,1);expSolution=[a,b];
@@ -78,27 +81,40 @@ classdef TestClass < matlab.unittest.TestCase
             actSolution=T.R(2,:,env.n);expSolution=[5*a,3*b];
             testCase.verifyEqual(actSolution, expSolution);
             
-            %test values that should be directly from input
+            %test values that should be transfered directly from input
             actSolution=T.Pi;expSolution=env.Pi;
             testCase.verifyEqual(actSolution, expSolution);
             actSolution=T.current;expSolution=env.init;
             testCase.verifyEqual(actSolution, expSolution);
             
             %test some edges
+            % edges should have value:
+            %l (left) if q2=q1-1 and mod(q1,5) not 1 (not at left edge)
             actSolution=full(T.adj(2,1));expSolution=l;
             testCase.verifyEqual(actSolution, expSolution);
+            %d (down) if q2=q1-5 and q1>5 (not at bottom)
             actSolution=full(T.adj(6,1));expSolution=d;
             testCase.verifyEqual(actSolution, expSolution);
+            %r (right) if q2=q1+1 and mod(q1,5) not 0 (not at right edge)
             actSolution=full(T.adj(14,15));expSolution=r;
             testCase.verifyEqual(actSolution, expSolution);
+            %u (up) if q2=q1+5 and q1<11 (not at top)
             actSolution=full(T.adj(2,7));expSolution=u;
             testCase.verifyEqual(actSolution, expSolution);
-             actSolution=full(T.adj(2,6));expSolution=0;
-             testCase.verifyEqual(actSolution, expSolution);
+            %0 otherwise (not possible to move from q1 to q2)
+            actSolution=full(T.adj(2,6));expSolution=0;
+            testCase.verifyEqual(actSolution, expSolution);
             
-            % test limits
+            % test limits FT which should describe the lines defining the 
+            %limits of the workspace on the form of two 2x3 matrix where
+            %each row is a limit line, the first element is the value of the
+            %parameter which is constant and the 2nd and 3rd is the start 
+            %and end values of the parameter that changes. Hence we expect
+            %FTi=[imin, jmin, jmax; imax, jmin, jmax]
+            % FT2 contains the lines along the x-axis (y constant)
             actSolution=T.FT{2};expSolution=[0, 0, 5*a; 3*b, 0, 5*a];
             testCase.verifyEqual(actSolution, expSolution);
+            %FT1 contains the lines along the y-axis (x constant)
             actSolution=T.FT{1};expSolution=[0, 0, 3*b; 5*a, 0, 3*b];
             testCase.verifyEqual(actSolution, expSolution);
         end
